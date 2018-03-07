5c438cd9c6c307da9da6e6fe9a984de01a4c300f
JT <certainthings@gmail.com>
2018-03-07 12:47:15 +1100

    my book list js dom example

---
+++ 03-dom/library/js/library.js
@@ -0,0 +1,15 @@
+const books = [{
+        title: 'The Design of EveryDay Things',
+        author: 'Don Norman',
+        alreadyRead: false
+    },
+    {
+        title: 'The Most Human Human',
+        author: 'Brian Christian',
+        alreadyRead: true
+    }
+];
+
+for (let i = 0; i < books.length; i++) {
+  console.log( books[i] );
+}
