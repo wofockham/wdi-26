b111c2e6b10899c1117653d35999dc735e5a6894
JT <certainthings@gmail.com>
2018-03-19 15:38:20 +1100

    ruby number guessing looping game

---
+++ 05-ruby/loops/guesser.rb
@@ -0,0 +1,36 @@
+# # Title: Guess The Number
+#
+# ### Activity:
+# - You are to generate a basic "guess my number" game.  The computer will pick a random number between 0 and 10.  The user will guess the number until they guess correctly.
+#
+# ### Specification:
+# - The user should be asked to guess a number
+# - If the user's guess is correct, the user should see a congratulatory message
+# - If the user's guess is not correct, the user should be asked to guess the number again.
+#
+# ### Extensions:
+# - Let the user choose the maximum value (so they can play a long game with a random value between 0 and 10000, for example).
+# - Give feedback to the user: "Wrong, guess higher!" or "Wrong, guess lower!"
+
+print "What is the maximum possible number you'd like to guess? "
+MAX_VALUE = gets.to_i
+
+SECRET_NUMBER = rand 0..MAX_VALUE
+debug_mode = false
+
+puts "The secret number is #{ SECRET_NUMBER }" if debug_mode
+
+print "Guess a secret number between 0 and #{ MAX_VALUE }: "
+guess = gets.to_i
+
+until guess == SECRET_NUMBER
+  print "Wrong, "
+  if guess > SECRET_NUMBER
+    print "guess lower: "
+  else
+    print "guess higher: "
+  end
+  guess = gets.to_i
+end
+
+puts "Congratulations, you got it"
