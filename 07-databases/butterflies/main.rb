b5d286342c2607c476482cc77f6c3c71e4cde19c
JT <certainthings@gmail.com>
2018-03-22 14:54:36 +1100

    working butterfly crud woo

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -9,10 +9,7 @@ end
 
 # Index
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-
-  @butterflies = db.execute 'SELECT * FROM butterflies'
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
@@ -23,19 +20,43 @@ end
 
 # Create
 post '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.execute "INSERT INTO butterflies (name, family, image) VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:image]}')"
+  query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:image]}')"
   redirect to('/butterflies') # GET request
 end
 
 # Show
 get '/butterflies/:id' do
-  # Fetch this butterfly from the database
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = @butterfly.first # Stripping away the outer array
+  erb :butterflies_show
+end
+
+# Edit
+get '/butterflies/:id/edit' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = @butterfly.first # Stripping away the outer array
+  erb :butterflies_edit
+end
+
+# Update
+post '/butterflies/:id' do
+  query_db "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
+  redirect to("/butterflies/#{ params[:id] }") # GET request
+end
+
+# Delete
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id = #{ params[:id] }"
+  redirect to('/butterflies')
+end
+
+def query_db(sql_statement)
   db = SQLite3::Database.new 'database.sqlite3'
   db.results_as_hash = true
 
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
-  @butterfly = @butterfly.first # Stripping away the outer array
+  puts sql_statement # Optional bonus
 
-  erb :butterflies_show
+  results = db.execute sql_statement
+  db.close
+  results
 end
