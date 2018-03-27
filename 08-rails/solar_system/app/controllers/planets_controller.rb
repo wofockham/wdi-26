b562a5bb25e45707bea6a68b969e643a1277f3d4
JT <certainthings@gmail.com>
2018-03-27 15:05:47 +1100

    solar system CRUD with rails

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -11,11 +11,30 @@ class PlanetsController < ApplicationController
   end
 
   def create
-    planet = Planet.create params[:planet]
+    planet = Planet.create planet_params
+    redirect_to planet_path(planet.id)
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.update planet_params
+    redirect_to planet_path(planet.id)
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path # index
   end
 
   private
   # Return a safe set of parameters (with anything bad taken out)
-  def planet_params
+  def planet_params # Strong params
+    # Whitelist
+    params.require(:planet).permit(:name, :image, :orbit, :diameter, :mass, :moons)
   end
 end
--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'planets#index'
   get '/planets' => 'planets#index' # planets_path
   get '/planets/new' => 'planets#new'
   post '/planets' => 'planets#create' # implicityly planets_path
   get '/planets/:id' => 'planets#show', :as => 'planet' # planet_path
+  get '/planets/:id/edit' => 'planets#edit', :as => 'planets_edit'
+  post '/planets/:id' => 'planets#update'
+  delete '/planets/:id' => 'planets#destroy'
 end
