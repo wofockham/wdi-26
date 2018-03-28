9c6269066e893f91bbd4d2befbdd489aa991788f
JT <certainthings@gmail.com>
2018-03-28 15:35:52 +1100

    works crud is done

--- 08-rails/moma/config/routes.rb
+++ 08-rails/moma/config/routes.rb
@@ -9,9 +9,18 @@
 #             PATCH  /artists/:id(.:format)      artists#update
 #             PUT    /artists/:id(.:format)      artists#update
 #             DELETE /artists/:id(.:format)      artists#destroy
+#       works GET    /works(.:format)            works#index
+#             POST   /works(.:format)            works#create
+#    new_work GET    /works/new(.:format)        works#new
+#   edit_work GET    /works/:id/edit(.:format)   works#edit
+#        work GET    /works/:id(.:format)        works#show
+#             PATCH  /works/:id(.:format)        works#update
+#             PUT    /works/:id(.:format)        works#update
+#             DELETE /works/:id(.:format)        works#destroy
 #
 
 Rails.application.routes.draw do
+  root :to => 'works#index'
   resources :artists # Plural
   resources :works
 end
