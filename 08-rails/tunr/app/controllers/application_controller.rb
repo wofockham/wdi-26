a4df5a13a3a8ab3461f4e4d886c251abfcb0d4b9
JT <certainthings@gmail.com>
2018-03-29 12:50:00 +1100

    sign in and sign out

--- 08-rails/tunr/app/controllers/application_controller.rb
+++ 08-rails/tunr/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :fetch_user
+
+  private
+  def fetch_user
+    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+    session[:user_id] = nil unless @current_user.present? # This prevents horrors if you reseed.
+  end
 end
--- 08-rails/tunr/config/routes.rb
+++ 08-rails/tunr/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
+
   resources :users, :only => [:new, :create]
+
+  get '/login' => 'session#new'        # Sign in form
+  post '/login' => 'session#create'    # Sign in action
+  delete '/login' => 'session#destroy' # Sign out
   # See rails guide for routing, in particular nested routes
 end
