ba3d2268f8df10e11eb7f01fce4df05873d22528
JT <certainthings@gmail.com>
2018-04-09 14:32:15 +1000

    numbers api via ajax

--- 10-ajax/ajax-intro/js/ajax-intro.js
+++ 10-ajax/ajax-intro/js/ajax-intro.js
@@ -1,13 +1,24 @@
 const fetchFact = function () {
   const xhr = new XMLHttpRequest();
+
+  // This function will be run several times during the
+  // request life cycle.
   xhr.onreadystatechange = function () {
+    // Ignore all the readyStates except 4 (complete).
     if (xhr.readyState !== 4) {
       return; // Not ready yet
     }
-    console.log( xhr.responseText );
+
+    const info = JSON.parse( xhr.responseText );
+    const p = document.createElement('p');
+    p.innerHTML = '<strong>' + info.number + '</strong>: ' + info.text;
+    document.body.appendChild( p );
   };
-  xhr.open('GET', 'http://numbersapi.com/random/trivia');
+
+  xhr.open('GET', 'http://numbersapi.com/random/trivia?json');
   xhr.send(); // Asynchronous
+
 };
 
-fetchFact();
+const button = document.getElementById('fetch');
+button.addEventListener('click', fetchFact);
