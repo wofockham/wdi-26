fe86ff8b0bb89c42bbbdb6803b2849a317bbeeee
JT <certainthings@gmail.com>
2018-04-10 11:38:34 +1000

    jquery google books ajax

--- 10-ajax/books/js/books.js
+++ 10-ajax/books/js/books.js
@@ -1,16 +1,16 @@
 const fetchBook = function (event) {
   event.preventDefault(); // Don't leave the page.
-  const query = document.getElementById('query').value;
+  const query = $('#query').val();
 
-  $.ajax(`https://www.googleapis.com/books/v1/volumes?q=${ query }&key=AIzaSyAAiCRdJuc3KbZmfKzLhPkm4DAoG5-7lcA`, {
-    success: function (info) {
-        const cover = info.items[0].volumeInfo.imageLinks.thumbnail;
-        const title = info.items[0].volumeInfo.title;
-        const image = document.getElementById('cover');
-        image.src = cover;
-        image.alt = title;
-    }
+  $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=${ query }&key=AIzaSyAAiCRdJuc3KbZmfKzLhPkm4DAoG5-7lcA`).done(function (info) {
+      const cover = info.items[0].volumeInfo.imageLinks.thumbnail;
+      const title = info.items[0].volumeInfo.title;
+      $('#cover').attr('src', cover).attr('alt', title);
+  }).fail(function () {
+    alert('Something bad happened');
   });
 }
 
-document.getElementById('title-search').addEventListener('submit', fetchBook);
+$(document).ready(function () {
+  $('#title-search').on('submit', fetchBook);
+});
