9a07b126008e62139d27169b55b37f52bed9621d
JT <certainthings@gmail.com>
2018-04-11 10:55:17 +1000

    manual throttling for ajax flickr

--- 10-ajax/flickr-search/js/search.js
+++ 10-ajax/flickr-search/js/search.js
@@ -1,5 +1,14 @@
+const state = {
+  page: 1,
+  requestInProgress: false,
+  lastPage: false
+};
+
 const searchFlickr = function (term) {
-  console.log('Searching Flickr for', term);
+  if (state.requestInProgress || state.lastPage) { return; }
+  state.requestInProgress = true;
+
+  console.log('Searching flickr for', term);
 
   // the jsoncallback=? here lets us use something called JSONP
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
@@ -10,8 +19,14 @@ const searchFlickr = function (term) {
     method: 'flickr.photos.search',
     api_key: '2f5ac274ecfac5a455f38745704ad084', // not a secret key
     text: term,
-    format: 'json'
-  }).done(showImages);
+    format: 'json',
+    page: state.page++
+  }).done( showImages ).done(function (r) {
+    state.requestInProgress = false;
+    if (r.photos.page >= r.photos.pages) {
+      state.lastPage = true;
+    }
+  });
 };
 
 const showImages = function (results) {
@@ -40,6 +55,9 @@ $(document).ready(function () {
   $('#search').on('submit', function (event) {
     event.preventDefault();
     const query = $('#query').val();
+    state.lastPage = false;
+    state.page = 1;
+    $('#images').empty();
     searchFlickr( query );
   });
 
@@ -59,8 +77,8 @@ $(document).ready(function () {
 
 /* TODO: ************************************************************
 
-throttle the requests -- don't make too many requests
 pagination -- eventually we should all possible matching results
+throttle the requests -- don't make too many requests
 stop at end of the results -- no more requests
 bonus: whatever you like
 
