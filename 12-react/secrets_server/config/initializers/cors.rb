1698fa11fea27c92fb5e3d13529812e1d5ad34e6
JT <certainthings@gmail.com>
2018-04-17 12:47:40 +1000

    secrets server

---
+++ 12-react/secrets_server/config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# This code was all jacked from the rack-cors github docs
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+      :headers => :any,
+      :methods => %i(get post put patch delete options head)
+  end
+end
