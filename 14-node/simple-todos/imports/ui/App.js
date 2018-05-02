b5989cf86b3e6749ae2ce0624e0a579426a9a049
JT <certainthings@gmail.com>
2018-05-02 14:05:53 +1000

    meteor + react tasks more or less complete

--- 14-node/simple-todos/imports/ui/App.js
+++ 14-node/simple-todos/imports/ui/App.js
@@ -1,18 +1,47 @@
 import React, { PureComponent as Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
 
-import Task from './Task';
+import { Tasks } from '../api/task'; // Mongodb collection (i.e. Model)
 
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' }
-    ]
+import Task from './Task'; // <Task> Component
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleHideCompleted = this.toggleHideCompleted.bind(this);
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim(); // ?!
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date()
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = ''; // ?!
   }
 
   renderTasks() {
-    return this.getTasks().map((task) => <Task key={task._id} task={task} />);
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.done);
+    }
+    return filteredTasks.map((task) => <Task key={task._id} task={task} />);
   }
 
   render() {
@@ -20,6 +49,16 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted} />
+            Hide completed tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+          </form>
+
         </header>
 
         <ul>
@@ -29,3 +68,10 @@ export default class App extends Component {
     )
   }
 }
+
+// Higher Order Function
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+  }
+})(App); // You are not expected to understand this.
--- 14-node/simple-todos/imports/ui/Task.js
+++ 14-node/simple-todos/imports/ui/Task.js
@@ -1,9 +1,33 @@
 import React, { PureComponent as Component } from 'react';
+import { Tasks } from '../api/task';
 
 export default class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.toggleDone = this.toggleDone.bind(this);
+  }
+
+  toggleDone() {
+    Tasks.update(this.props.task._id, {
+      $set: { done: !this.props.task.done }
+    })
+  }
+
+  deleteTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.done ? "checked" : "";
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteTask}>
+          &times;
+        </button>
+        <input type="checkbox" readOnly checked={!!this.props.task.done} onClick={this.toggleDone} />
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
