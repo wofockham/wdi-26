e6a67258f46778b1f06578b8ce33921327f32d44
JT <certainthings@gmail.com>
2018-05-02 16:16:35 +1000

    better annotation of fileIO

--- 14-node/intro/fileIO.js
+++ 14-node/intro/fileIO.js
@@ -1,18 +1,7 @@
 const fs = require('fs');
 
-// Async: the callback will almost always receive params:
-// error, data
-fs.readFile('README.txt', 'utf-8', (error, data) => {
-  if (error) {
-    return console.error(error);
-  }
-
-  console.log( data );
-});
-
-console.log('program finished');
-
-// Synchronous
+// Synchronous: you have to catch the error yourself
+// with Javascript's try/catch.
 try {
   const data = fs.readFileSync('README.txxxt', 'utf-8');
   console.log( data );
@@ -21,3 +10,28 @@ try {
   console.log(error);
   console.log('exiting gracefully');
 }
+
+// Asynchronous: the callback will generally receive:
+// error and data params
+// Checking for and handling the error is your responsibility.
+fs.readFile('README.txt', 'utf-8', (error, data) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  console.log( data );
+});
+
+// You can convert any callback function to a Promise
+// returning function like so:
+const readFile = function (filename) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filename, 'utf-8', function (error, data) {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(data);
+    });
+  });
+};
