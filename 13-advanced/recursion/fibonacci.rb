d926b4d31dafd5dfd99fa310d9807a1815fa40c2
JT <certainthings@gmail.com>
2018-05-10 15:25:43 +1000

    fibonacci recursive in two flavours

--- 13-advanced/recursion/fibonacci.rb
+++ 13-advanced/recursion/fibonacci.rb
@@ -9,8 +9,20 @@ def fibonacci_iterative(n)
   a
 end
 
-def fibonacci_recursive(n)
-  # Homework
+def fibonacci_recursive_slow(n)
+  if n == 1 || n == 2
+    1
+  else
+    fibonacci_recursive_slow(n - 1) + fibonacci_recursive_slow(n - 2)
+  end
+end
+
+def fib(n, a=1, b=1)
+  if n == 1 || n == 2
+    b
+  else
+    fib(n-1, b, a+b) # Move closer to the base case
+  end
 end
 
 require 'pry'
