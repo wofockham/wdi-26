3021331bb63042c5df5a0a6f5ddf8ba3cdf88f12
JT <certainthings@gmail.com>
2018-05-11 12:00:55 +1000

    gatsby with typography.js

---
+++ 12-react/gatsby-intro/tutorial-part-two/src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default () => (
+  <div style={ {margin: "3em auto", maxWidth: 600} }>
+    <h1>Mitch Hedberg on Drugs</h1>
+    <div>
+      <blockquote>
+        <p>
+          I used to do drugs. I still do, but I used to as well.
+          <br />
+          <em>(The irony is he died of a drug overdose.)</em>
+        </p>
+      </blockquote>
+    </div>
+    <p>Posted May 11, 2018</p>
+  </div>
+);
---
+++ 12-react/gatsby-intro/tutorial-part-two/src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from 'typography';
+// import bootstrapTheme from 'typography-theme-bootstrap';
+import lawtonTheme from 'typography-theme-lawton';
+
+// const typography = new Typography({ baseFontSize: '8px' });
+// const typography = new Typography(bootstrapTheme);
+const typography = new Typography(lawtonTheme);
+
+export default typography;
