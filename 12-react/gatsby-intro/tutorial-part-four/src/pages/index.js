dbca3a0ad90ccb317b3e5fef751bad4a5bf3b1ac
JT <certainthings@gmail.com>
2018-05-11 14:42:21 +1000

    graphql + gatsby skellington

---
+++ 12-react/gatsby-intro/tutorial-part-four/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>Bill Murray Fansite</h1>
+    <div>
+      <img src="http://fillmurray.com/800/1200" />
+    </div>
+  </div>
+);
